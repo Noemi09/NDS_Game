df1b896b0c57f5615b8a94361d315f5b49b4bd40
Noemidev <90848793+Noemi09@users.noreply.github.com>
2024-02-10 03:25:10 +0100

    Uploading the whole project

---
+++ build/proyectofinal.d
@@ -0,0 +1,130 @@
+proyectofinal.o: \
+ C:/NDSGameNoemiVillarroya/proyect_with_music/source/proyectofinal.c \
+ C:/DevkitPro/libnds/include/nds.h \
+ C:/DevkitPro/libnds/include/nds/libversion.h \
+ C:/DevkitPro/libnds/include/nds/ndstypes.h \
+ C:/DevkitPro/libnds/include/nds/bios.h \
+ C:/DevkitPro/libnds/include/nds/ndstypes.h \
+ C:/DevkitPro/libnds/include/nds/card.h \
+ C:/DevkitPro/libnds/include/nds/ndstypes.h \
+ C:/DevkitPro/libnds/include/nds/debug.h \
+ C:/DevkitPro/libnds/include/nds/dma.h \
+ C:/DevkitPro/libnds/include/nds/interrupts.h \
+ C:/DevkitPro/libnds/include/nds/ipc.h \
+ C:/DevkitPro/libnds/include/nds/memory.h \
+ C:/DevkitPro/libnds/include/nds/system.h \
+ C:/DevkitPro/libnds/include/nds/timers.h \
+ C:/DevkitPro/libnds/include/nds/fifocommon.h \
+ C:/DevkitPro/libnds/include/nds/interrupts.h \
+ C:/DevkitPro/libnds/include/nds/touch.h \
+ C:/DevkitPro/libnds/include/nds/input.h \
+ C:/DevkitPro/libnds/include/nds/sha1.h \
+ C:/DevkitPro/libnds/include/nds/arm9/dynamicArray.h \
+ C:/DevkitPro/libnds/include/nds/arm9/linkedlist.h \
+ C:/DevkitPro/libnds/include/nds/arm9/background.h \
+ C:/DevkitPro/libnds/include/nds/arm9/video.h \
+ C:/DevkitPro/libnds/include/nds/arm9/sassert.h \
+ C:/DevkitPro/libnds/include/nds/memory.h \
+ C:/DevkitPro/libnds/include/nds/dma.h \
+ C:/DevkitPro/libnds/include/nds/arm9/boxtest.h \
+ C:/DevkitPro/libnds/include/nds/arm9/videoGL.h \
+ C:/DevkitPro/libnds/include/nds/arm9/cache.h \
+ C:/DevkitPro/libnds/include/nds/arm9/trig_lut.h \
+ C:/DevkitPro/libnds/include/nds/arm9/math.h \
+ C:/DevkitPro/libnds/include/nds/arm9/dynamicArray.h \
+ C:/DevkitPro/libnds/include/nds/arm9/cache.h \
+ C:/DevkitPro/libnds/include/nds/arm9/console.h \
+ C:/DevkitPro/libnds/include/nds/arm9/background.h \
+ C:/DevkitPro/libnds/include/nds/arm9/decompress.h \
+ C:/DevkitPro/libnds/include/nds/bios.h \
+ C:/DevkitPro/libnds/include/nds/arm9/exceptions.h \
+ C:/DevkitPro/libnds/include/nds/arm9/guitarGrip.h \
+ C:/DevkitPro/libnds/include/nds/arm9/image.h \
+ C:/DevkitPro/libnds/include/nds/arm9/pcx.h \
+ C:/DevkitPro/libnds/include/nds/arm9/input.h \
+ C:/DevkitPro/libnds/include/nds/touch.h \
+ C:/DevkitPro/libnds/include/nds/input.h \
+ C:/DevkitPro/libnds/include/nds/arm9/keyboard.h \
+ C:/DevkitPro/libnds/include/nds/arm9/math.h \
+ C:/DevkitPro/libnds/include/nds/arm9/paddle.h \
+ C:/DevkitPro/libnds/include/nds/arm9/pcx.h \
+ C:/DevkitPro/libnds/include/nds/arm9/piano.h \
+ C:/DevkitPro/libnds/include/nds/arm9/rumble.h \
+ C:/DevkitPro/libnds/include/nds/arm9/sassert.h \
+ C:/DevkitPro/libnds/include/nds/arm9/sound.h \
+ C:/DevkitPro/libnds/include/nds/arm9/sprite.h \
+ C:/DevkitPro/libnds/include/nds/system.h \
+ C:/DevkitPro/libnds/include/nds/arm9/window.h \
+ C:/DevkitPro/libnds/include/nds/arm9/sprite.h \
+ C:/DevkitPro/libnds/include/nds/arm9/trig_lut.h \
+ C:/DevkitPro/libnds/include/nds/arm9/video.h \
+ C:/DevkitPro/libnds/include/nds/arm9/videoGL.h \
+ C:/DevkitPro/libnds/include/nds/arm9/nand.h \
+ C:/DevkitPro/libnds/include/maxmod9.h \
+ C:/DevkitPro/libnds/include/mm_types.h \
+ C:/NDSGameNoemiVillarroya/proyect_with_music/build/soundbank.h \
+ C:/NDSGameNoemiVillarroya/proyect_with_music/build/soundbank_bin.h
+C:/DevkitPro/libnds/include/nds.h:
+C:/DevkitPro/libnds/include/nds/libversion.h:
+C:/DevkitPro/libnds/include/nds/ndstypes.h:
+C:/DevkitPro/libnds/include/nds/bios.h:
+C:/DevkitPro/libnds/include/nds/ndstypes.h:
+C:/DevkitPro/libnds/include/nds/card.h:
+C:/DevkitPro/libnds/include/nds/ndstypes.h:
+C:/DevkitPro/libnds/include/nds/debug.h:
+C:/DevkitPro/libnds/include/nds/dma.h:
+C:/DevkitPro/libnds/include/nds/interrupts.h:
+C:/DevkitPro/libnds/include/nds/ipc.h:
+C:/DevkitPro/libnds/include/nds/memory.h:
+C:/DevkitPro/libnds/include/nds/system.h:
+C:/DevkitPro/libnds/include/nds/timers.h:
+C:/DevkitPro/libnds/include/nds/fifocommon.h:
+C:/DevkitPro/libnds/include/nds/interrupts.h:
+C:/DevkitPro/libnds/include/nds/touch.h:
+C:/DevkitPro/libnds/include/nds/input.h:
+C:/DevkitPro/libnds/include/nds/sha1.h:
+C:/DevkitPro/libnds/include/nds/arm9/dynamicArray.h:
+C:/DevkitPro/libnds/include/nds/arm9/linkedlist.h:
+C:/DevkitPro/libnds/include/nds/arm9/background.h:
+C:/DevkitPro/libnds/include/nds/arm9/video.h:
+C:/DevkitPro/libnds/include/nds/arm9/sassert.h:
+C:/DevkitPro/libnds/include/nds/memory.h:
+C:/DevkitPro/libnds/include/nds/dma.h:
+C:/DevkitPro/libnds/include/nds/arm9/boxtest.h:
+C:/DevkitPro/libnds/include/nds/arm9/videoGL.h:
+C:/DevkitPro/libnds/include/nds/arm9/cache.h:
+C:/DevkitPro/libnds/include/nds/arm9/trig_lut.h:
+C:/DevkitPro/libnds/include/nds/arm9/math.h:
+C:/DevkitPro/libnds/include/nds/arm9/dynamicArray.h:
+C:/DevkitPro/libnds/include/nds/arm9/cache.h:
+C:/DevkitPro/libnds/include/nds/arm9/console.h:
+C:/DevkitPro/libnds/include/nds/arm9/background.h:
+C:/DevkitPro/libnds/include/nds/arm9/decompress.h:
+C:/DevkitPro/libnds/include/nds/bios.h:
+C:/DevkitPro/libnds/include/nds/arm9/exceptions.h:
+C:/DevkitPro/libnds/include/nds/arm9/guitarGrip.h:
+C:/DevkitPro/libnds/include/nds/arm9/image.h:
+C:/DevkitPro/libnds/include/nds/arm9/pcx.h:
+C:/DevkitPro/libnds/include/nds/arm9/input.h:
+C:/DevkitPro/libnds/include/nds/touch.h:
+C:/DevkitPro/libnds/include/nds/input.h:
+C:/DevkitPro/libnds/include/nds/arm9/keyboard.h:
+C:/DevkitPro/libnds/include/nds/arm9/math.h:
+C:/DevkitPro/libnds/include/nds/arm9/paddle.h:
+C:/DevkitPro/libnds/include/nds/arm9/pcx.h:
+C:/DevkitPro/libnds/include/nds/arm9/piano.h:
+C:/DevkitPro/libnds/include/nds/arm9/rumble.h:
+C:/DevkitPro/libnds/include/nds/arm9/sassert.h:
+C:/DevkitPro/libnds/include/nds/arm9/sound.h:
+C:/DevkitPro/libnds/include/nds/arm9/sprite.h:
+C:/DevkitPro/libnds/include/nds/system.h:
+C:/DevkitPro/libnds/include/nds/arm9/window.h:
+C:/DevkitPro/libnds/include/nds/arm9/sprite.h:
+C:/DevkitPro/libnds/include/nds/arm9/trig_lut.h:
+C:/DevkitPro/libnds/include/nds/arm9/video.h:
+C:/DevkitPro/libnds/include/nds/arm9/videoGL.h:
+C:/DevkitPro/libnds/include/nds/arm9/nand.h:
+C:/DevkitPro/libnds/include/maxmod9.h:
+C:/DevkitPro/libnds/include/mm_types.h:
+C:/NDSGameNoemiVillarroya/proyect_with_music/build/soundbank.h:
+C:/NDSGameNoemiVillarroya/proyect_with_music/build/soundbank_bin.h:
---
+++ build/soundbank.h
@@ -0,0 +1,11 @@
+#define SFX_RETRETE	0
+#define SFX_THEWITCHER	1
+#define SFX_COCINAR	2
+#define SFX_DUCHA	3
+#define SFX_RONQUIDO	4
+#define MOD_SAD	0
+#define MOD_SIMCITY	1
+#define SFX_VOCES	37
+#define MSL_NSONGS	2
+#define MSL_NSAMPS	38
+#define MSL_BANKSIZE	40
